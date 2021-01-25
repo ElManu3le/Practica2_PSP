dacb0be63d6ee7075955ca983fc77a8da2a6327d
ElManu3le <62712661+ElManu3le@users.noreply.github.com>
2021-01-25 19:29:10 +0100

    Start

---
+++ Practica2/src/main/java/Practica2PSP/App.java
@@ -0,0 +1,13 @@
+package Practica2PSP;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello World!" );
+    }
+}
