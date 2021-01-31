f1e1891f7c32ff3fdf23e0e95ceacd77555c8289
ElManu3le <msancho620@yahoo.es>
2021-01-31 18:43:28 +0100

    cosa

---
+++ Practica2/src/main/java/Practica2/App.java
@@ -0,0 +1,13 @@
+package Practica2;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello World!" );
+    }
+}
